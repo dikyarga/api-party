d5af6709e3f807cf9e01743acfcd89f176d62531
Diky Arga <dikyarga.id@gmail.com>
2017-02-28 16:33:24 +0700

    update small part

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 let twitterController = require('../controllers/twitter')
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
